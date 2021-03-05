cee6c8b5c0d037479ff0afb1d823cab274b67523
istiak ahmed ovi <istiak6069@gmail.com>
2021-03-05 10:13:00 +0600

    Add solution for problem `A - Puzzles` Link: https://codeforces.com/contest/337/problem/A Tags: greedy, *900 Ref: https://codeforces.com/contest/337/submission/109122030

---
+++ codeforces/337/A.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main ()
+{
+  int st,puz;
+  cin>>st>>puz;
+  int i,ara[puz];
+  for(i=0;i<puz;i++){
+    cin>>ara[i];
+  }
+  sort(ara,ara+puz);
+  int mi=1000000,r,c;
+  for(i=0;i<puz;i++){
+    c=ara[st-1],
+   r=c-ara[i];
+   if(r<mi){
+    mi=r;
+   }
+   if(st-1==puz-1){
+    break;
+   }
+   st++;
+  }
+cout<<mi<<endl;
+    return 0;
+}
