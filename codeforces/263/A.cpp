b31de2426a8725c3e8145e3a33f4b276a05464bd
istiak ahmed ovi <istiak6069@gmail.com>
2021-03-08 13:22:00 +0600

    Add solution for problem `A - Beautiful Matrix` Link: https://codeforces.com/contest/263/problem/A Tags: implementation, *800 Ref: https://codeforces.com/contest/263/submission/109378514

--- codeforces/263/A.cpp
+++ codeforces/263/A.cpp
@@ -2,15 +2,22 @@
 using namespace std;
 int main()
 {
-    int i,j,ara[5][5],x,y;
-    for(i=0; i<5; i++)
-    {
-        for(j=0; j<5; j++)
-        {
-            cin>>ara[i][j];
-            if(ara[i][j]==1)
-                x=i,y=j;
+int i,j,ara[5][5];
+for(i=0;i<5;i++){
+    for(j=0;j<5;j++){
+        cin>>ara[i][j];
+    }
+}
+int x,y;
+for(i=0;i<5;i++){
+    for(j=0;j<5;j++){
+        if(ara[i][j]==1){
+            x=i;
+            y=j;
         }
     }
-    cout<<abs(2-x)+abs(2-y)<<endl;
+}
+int r1=abs(2-x);
+int r2=abs(2-y);
+cout<<r1+r2<<endl;
 }
