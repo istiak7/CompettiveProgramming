b20d18f8e949bfe14a9cb371b4231e0cd1d7f2ee
istiak ahmed ovi <istiak6069@gmail.com>
2021-03-19 15:08:00 +0600

    Add solution for problem `A - King Moves` Link: https://codeforces.com/contest/710/problem/A Tags: implementation, *800 Ref: https://codeforces.com/contest/710/submission/110433427

--- codeforces/710/A.cpp
+++ codeforces/710/A.cpp
@@ -2,30 +2,52 @@
 using namespace std;
 int main()
 {
+    int ara[200][8];
 
-	string s;
-	cin >> s;
+    int i,j;
+    for(j=97; j<105; j++)
+    {
+        for(i=1; i<9; i++)
+        {
+            if(i==1&&j==97||i==1&&j==104)
+                ara[j][i]=3;
 
-	if(s[0] == 'a' || s[0] == 'h')
-	{
-		if(s[1] == '8' || s[1] == '1')
-		{
-			cout << 3;
-		}
-		else
-		{
-			cout << 5;
-		}
-	}
-	else
-	{
-		if(s[1] == '8' || s[1] == '1')
-		{
-			cout << 5;
-		}
-		else
-			cout << 8;
-	}
-    return 0;
+            else if(i==1&&j!=97||i==1&&j!=104)
+                ara[j][i]=5;
+            else if(i==8&&j!=97&&j!=104)
+                ara[j][i]=5;
+
+
+            else  if(i==8&&j==104||i==8&&j==97)
+                ara[j][i]=3;
+            else if(j==97||j==104)
+                ara[j][i]=5;
+            else
+                ara[j][i]=8;
+        }
+    }
+    char a;
+    int c,b;
+    cin>>a>>b;
+    if(a=='a')
+        c=97;
+    if(a=='b')
+        c=98;
+    if(a=='c')
+        c=99;
+    if(a=='d')
+        c=100;
+    if(a=='e')
+        c=101;
+    if(a=='f')
+        c=102;
+    if(a=='g')
+        c=103;
+    if(a=='h')
+        c=104;
+
+
+    cout<<ara[c][b];
+
 }
 
