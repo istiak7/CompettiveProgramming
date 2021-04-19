c8dac8ca176a705569c41d4e8afbd954802cc261
istiak ahmed ovi <istiak6069@gmail.com>
2021-04-19 14:49:00 +0600

    Add solution for problem `C - Lucky Numbers` Link: https://codeforces.com/contest/630/problem/C Tags: combinatorics, math, *1100 Ref: https://codeforces.com/contest/630/submission/113469865

---
+++ codeforces/630/C.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n,i;
+    cin>>n;
+    long long int ans=0;
+        ans+=pow(2,(n+1))-2;
+
+    cout<<ans<<endl;
+    return 0;
+}
