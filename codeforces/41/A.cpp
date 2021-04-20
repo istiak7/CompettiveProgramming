6b3955f0d3472722cf360815300d5747e7e8baaf
istiak ahmed ovi <istiak6069@gmail.com>
2021-04-20 23:43:00 +0600

    Add solution for problem `A - Translation` Link: https://codeforces.com/contest/41/problem/A Tags: implementation, strings, *800 Ref: https://codeforces.com/contest/41/submission/113641065

--- codeforces/41/A.cpp
+++ codeforces/41/A.cpp
@@ -1,25 +1,13 @@
-#include <bits/stdc++.h>
-#define ll long long int
-#define pb push_back
+#include<bits/stdc++.h>
 using namespace std;
-
 int main()
 {
-    // freopen("in.txt","r",stdin);
-    // freopen("out.txt","w",stdout);
-
-    string s1,s2;
-    char x[100];
-    cin>>s1>>s2;
-    int l=s1.length()-1;
-    int i,j=0;
-    for(i=l; i>=0; i--)
-    {
-        x[j]=s2[i];
-        j++;
-    }
-    x[j]='\0';
-    if(s1==x)cout<<"YES"<<endl;
-    else cout<<"NO"<<endl;
+   string s1,s2;
+   cin>>s1>>s2;
+   reverse(s2.begin(),s2.end());
+   if(s1==s2)
+    cout<<"YES"<<endl;
+   else
+    cout<<"NO"<<endl;
     return 0;
 }
