f30386067fb71cf07a8f11154cab77526346c673
istiak ahmed ovi <istiak6069@gmail.com>
2021-05-29 20:08:00 +0600

    Add solution for problem `A - Face Detection` Link: https://codeforces.com/contest/549/problem/A Tags: implementation, strings, *900 Ref: https://codeforces.com/contest/549/submission/117781813

--- codeforces/549/A.cpp
+++ codeforces/549/A.cpp
@@ -5,14 +5,16 @@ using namespace std;
 
 int main()
 {
-    // freopen("in.txt","r",stdin);
+//    freopen("in.txt","r",stdin);
     // freopen("out.txt","w",stdout);
     int n,m;
     cin>>n>>m;
 
     char ara[n+1][m+1];
-    int i,j,cnt=0;
-    int ara2[125]= {0};
+    char a,b,c,d,e;
+    int i,j,flag,cnt=0;
+    int ara2[125];
+    ara2[97]=ara2[99]=ara2[101]=ara2[102]=0;
     for(i=0; i<n; i++)
     {
         for(j=0; j<m; j++)
@@ -25,17 +27,21 @@ int main()
 
         for(j=0; j<m; j++)
         {
-            if(i!=n-1&&j!=m-1)
-            {
+               flag=0;
+            if(i!=n-1&&j!=m-1){
+          
+            ara2[ ara[i][j] ]++;
+            ara2[ ara[i][j+1] ]++;
+            ara2[ ara[i+1][j] ]++;
+            ara2[ ara[i+1][j+1] ]++;
+            
+                   if(ara2[97]==1&& ara2[99]==1 && ara2[101]==1&&ara2[102]==1)
+                    flag=1;
 
-                ara2[ ara[i][j] ]++;
-                ara2[ ara[i][j+1] ]++;
-                ara2[ ara[i+1][j] ]++;
-                ara2[ ara[i+1][j+1] ]++;
+        if(flag==1)cnt++;
+          ara2[97]=ara2[99]=ara2[101]=ara2[102]=0;
 
-                if(ara2[97]==1&& ara2[99]==1 && ara2[101]==1&&ara2[102]==1)cnt++;
-                ara2[97]= ara2[99]=ara2[101]=ara2[102]=0;
-            }
+        }
         }
     }
     cout<<cnt<<endl;
