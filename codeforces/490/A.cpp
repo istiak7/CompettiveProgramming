d0b05c6102e2e5ecf8bc28f2c14486e19441751a
istiak ahmed ovi <istiak6069@gmail.com>
2021-06-03 13:49:00 +0600

    Add solution for problem `A - Team Olympiad` Link: https://codeforces.com/contest/490/problem/A Tags: greedy, implementation, sortings, *800 Ref: https://codeforces.com/contest/490/submission/118254650

--- codeforces/490/A.cpp
+++ codeforces/490/A.cpp
@@ -7,23 +7,40 @@ int main()
 {
     // freopen("in.txt","r",stdin);
     // freopen("out.txt","w",stdout);
-    ll n,x;
-    cin>>n;
-    vector<int>vect1,vect2,vect3;
-    for(int i=0; i<n; i++)
-    {
-        cin>>x;
-        if(x==1)vect1.push_back(i+1);
-        else if(x==2)vect2.push_back(i+1);
-        else vect3.push_back(i+1);
-    }
-    int minlen=min(vect1.size(),(min(vect2.size(),vect3.size())));
-    cout<<minlen<<endl;
-    int i=0;
-    while(minlen>0)
-    {
-        cout<<vect1[i]<<" "<<vect2[i]<<" "<<vect3[i]<<endl;
-        i++;
-        minlen--;
-    }
+  int n,c1=0,c2=0,c3=0;
+  int flag1,flag2,flag3;
+  cin>>n;
+  int ara[n];
+   int i=0;
+   for(i=0;i<n;i++){
+       cin>>ara[i];
+       if(ara[i]==1)c1++;
+       else if(ara[i]==2)c2++;
+       else if(ara[i]==3)c3++;
+   }
+   int mi=min(c3,(min(c1,c2)));
+   cout<<mi<<endl;
+   while(mi--){
+  flag1=flag2=flag3=0;
+      for(i=0;i<n;i++){
+
+            if(ara[i]==1&&flag1==0){
+                    ara[i]=0;
+                cout<<i+1<<" ";
+                flag1=1;
+            }
+            else if(ara[i]==2&&flag2==0){
+                ara[i]=0;
+                cout<<i+1<<" ";
+                flag2=1;
+            }
+            else if(ara[i]==3&&flag3==0){
+                ara[i]=0;
+                cout<<i+1<<" ";
+                flag3=1;
+            }
+
+       }
+       cout<<endl;
+   }
 }
