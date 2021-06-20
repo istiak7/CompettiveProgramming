0dd5dac6c4df46b610cb749ebeb31ce56c88ab87
istiak ahmed ovi <istiak6069@gmail.com>
2021-06-20 13:21:00 +0600

    Add solution for problem `A - Kefa and First Steps` Link: https://codeforces.com/contest/580/problem/A Tags: brute force, dp, implementation, *900 Ref: https://codeforces.com/contest/580/submission/120048270

---
+++ codeforces/580/A.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+#define ll long long int
+#define pb push_back
+using namespace std;
+
+int main()
+{
+    // freopen("in.txt","r",stdin);
+    // freopen("out.txt","w",stdout);
+    ll n;
+    cin>>n;
+    vector<int>v;
+    int i;
+    for(i=0; i<n; i++)
+    {
+        ll x;
+        cin>>x;
+        v.push_back(x);
+    }
+    int mx=0,cnt=0;
+    for(i=0; i<v.size()-1; i++)
+    {
+        if(v[i]<=v[i+1])
+        {
+            cnt++;
+            if(mx<cnt)mx=cnt;
+        }
+        else cnt=0;
+    }
+    cout<<mx+1<<endl;
+    return 0;
+}
