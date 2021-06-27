5832ce0aafb087206edc3aa6b453383a745b49c3
istiak ahmed ovi <istiak6069@gmail.com>
2021-06-27 23:44:00 +0600

    Add solution for problem `A - Lucky Division` Link: https://codeforces.com/contest/122/problem/A Tags: brute force, number theory, *1000 Ref: https://codeforces.com/contest/122/submission/120754880

--- codeforces/122/A.cpp
+++ codeforces/122/A.cpp
@@ -7,18 +7,17 @@ int main()
 {
     // freopen("in.txt","r",stdin);
     // freopen("out.txt","w",stdout);
-    ll n;
-    cin>>n;
-    int i;
-    vector<int>v= {4,7,47,74,444,447,474,477,747,774,777};
-    for(i=0; i<v.size(); i++)
+    string s;
+    cin>>s;
+    int i,cnt=0;
+    for(i=0; i<s.size(); i++)
     {
-        if(n%v[i]==0)
-        {
-            cout<<"YES"<<endl;
-            return 0;
-        }
+        if(s[i]=='4'||s[i]=='7')cnt++;
     }
-    cout<<"NO"<<endl;
+    int num=stoi(s);
+    if(s.size()==cnt)cout<<"YES"<<endl;
+
+    else if(num%4==0||num%7==0||num%47==0||num%74==0||num%444==0||num%447==0||num%474==0||num%477==0||num%747==0||num%774==0||num%777==0)cout<<"YES"<<endl;
+    else cout<<"NO"<<endl;
     return 0;
 }
