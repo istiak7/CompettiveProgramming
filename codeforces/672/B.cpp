f81aed06ad40359853a7bc21151a0f1d5fd8cde4
istiak ahmed ovi <istiak6069@gmail.com>
2021-07-16 21:48:00 +0600

    Add solution for problem `B - Different is Good` Link: https://codeforces.com/contest/672/problem/B Tags: constructive algorithms, implementation, strings, *1000 Ref: https://codeforces.com/contest/672/submission/122693725

---
+++ codeforces/672/B.cpp
@@ -0,0 +1,22 @@
+///*BISMILLAHIR RAHMANIR RAHIM*///
+#include <bits/stdc++.h>
+#define ll long long int
+#define pb push_back
+#define optimize() ios_base ::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+using namespace std;
+int main()
+{
+    optimize();
+    int n;
+    cin>>n;
+    string s;
+    cin>>s;
+    sort(s.begin(),s.end());
+    int cnt=0;
+    for(int i=0; i<s.size()-1; i++)if(s[i]==s[i+1])cnt++;
+    if(n>26)cout<<"-1"<<endl;
+    else cout<<cnt<<endl;
+    return 0;
+}
+
+
