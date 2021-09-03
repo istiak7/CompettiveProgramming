c95a58ee9477b8edaa1a56c6bfb1bbc6ed49365d
istiak ahmed ovi <istiak6069@gmail.com>
2021-09-03 12:04:00 +0600

    Add solution for problem `B - Lovely Palindromes` Link: https://codeforces.com/contest/688/problem/B Tags: constructive algorithms, math, *1000 Ref: https://codeforces.com/contest/688/submission/127741687

---
+++ codeforces/688/B.cpp
@@ -0,0 +1,31 @@
+///*BISMILLAHIR RAHMANIR RAHIM*///
+#include <bits/stdc++.h>
+#define ll long long int
+#define pb push_back
+#define optimize() ios_base :: sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define max_size 200005
+
+using namespace std;
+// freopen("input.txt","r",stdin);
+// freopen("output.txt","w",stdout);
+
+/*
+bool isPrime(int number){
+ bool flag=true;
+ for(int l= 2; l<= number / 2; l++) {
+ if(number % l == 0) {
+     flag = false;
+     break;
+    }
+ }
+ return flag;
+} */
+int main(){
+string s;
+cin>>s;
+cout<<s;
+reverse(s.begin(),s.end());
+cout<<s;
+}
+
+
