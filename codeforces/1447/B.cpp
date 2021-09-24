c28b452ad5c92f65d938007567b2f6996aa26c13
istiak ahmed ovi <istiak6069@gmail.com>
2021-09-24 14:32:00 +0600

    Add solution for problem `B - Numbers Box` Link: https://codeforces.com/contest/1447/problem/B Tags: greedy, math, *1000 Ref: https://codeforces.com/contest/1447/submission/129740020

---
+++ codeforces/1447/B.cpp
@@ -0,0 +1,49 @@
+///*BISMILLAHIR RAHMANIR RAHIM*///
+#include <bits/stdc++.h>
+#define ll long long int
+#define pb push_back
+#define optimize() ios_base :: sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define max_size 200005
+using namespace std;
+// freopen("input.txt","r",stdin);
+// freopen("output.txt","w",stdout);
+
+/*
+bool isPrime(int number){
+ bool flag=true;
+ for(int l= 2; l<= number / 2; l++) {
+ if(number % l == 0) {
+     flag = false;
+     break;
+    }
+ }
+ return flag;
+} */
+ int main()
+ {
+    optimize();
+    int t,x;cin>>t;
+    while(t--){
+        int n,m;cin>>n>>m;
+        int i,j;
+        int flag=1,cnt=0,sum=0,mini=101;
+        for(i=0;i<n;i++){
+            for(j=0;j<m;j++){
+                cin>>x;
+                mini=min(mini,abs(x));
+                if(x==0)flag=0;
+                if(x<0)cnt++;
+                sum+=abs(x);
+            }
+        }
+
+        if(flag==0||cnt%2==0)
+            cout<<sum<<endl;
+        else
+         cout<<sum  - (2*mini)<<endl;
+    }
+
+ }
+
+
+
