cabece5ea2caae0d3415a1afc3ac280cbc43272a
istiak ahmed ovi <istiak6069@gmail.com>
2021-11-09 10:27:00 +0600

    Add solution for problem `B - Caisa and Pylons` Link: https://codeforces.com/contest/463/problem/B Tags: brute force, implementation, math, *1100 Ref: https://codeforces.com/contest/463/submission/134720816

---
+++ codeforces/463/B.cpp
@@ -0,0 +1,27 @@
+///*BISMILLAHIR RAHMANIR RAHIM*///
+#include <bits/stdc++.h>
+#define ll long long int
+#define pb push_back
+#define endl '\n'
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+#define optimize() ios_base :: sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define max_size 1000001
+using namespace std;
+// freopen("input.txt","r",stdin);
+// freopen("output.txt","w",stdout);
+
+/*bool isprime(ll n ){
+       for(ll i=2;i*i<=n;i++){
+        if(n%i==0)return false;
+       }
+       return true;
+}
+*/
+int main(){
+    int n;cin>>n;
+    int ara[n];
+    for(int i=0;i<n;i++)cin>>ara[i];
+    sort(ara,ara+n);
+    cout<<ara[n-1]<<endl;
+}
+
