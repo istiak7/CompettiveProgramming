17bd55930feb7056c595db4323eb2dbd98c5cb56
istiak ahmed ovi <istiak6069@gmail.com>
2021-11-16 11:26:00 +0600

    Add solution for problem `B - Weird Subtraction Process` Link: https://codeforces.com/contest/946/problem/B Tags: math, number theory, *1100 Ref: https://codeforces.com/contest/946/submission/135828499

---
+++ codeforces/946/B.cpp
@@ -0,0 +1,29 @@
+///*BISMILLAHIR RAHMANIR RAHIM*///
+#include <bits/stdc++.h>
+#define ll long long int
+#define pb push_back
+#define endl '\n'
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+#define optimize() ios_base :: sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define max_size 1000001
+using namespace std;
+// freopen("input.txt","r",stdin);
+// freopen("output.txt","w",stdout);
+
+/*bool isprime(ll n ){
+       for(ll i=2;i*i<=n;i++){
+        if(n%i==0)return false;
+       }
+       return true;
+}
+*/
+int main(){
+    ll a,b;cin>>a>>b;
+  while(a>0&&b>0){
+    if(a>=2*b)a%=2*b;
+    else if(b>=2*a)b%=2*a;
+    else break;
+  } cout<<a<<" "<<b<<'\n';
+}
+
+
