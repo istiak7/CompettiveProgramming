b8a65e8f0f8ac64cb12bd390a158c56545e75296
istiak ahmed ovi <istiak6069@gmail.com>
2021-12-15 21:54:00 +0600

    Add solution for problem `C - K-th Not Divisible by n` Link: https://codeforces.com/contest/1352/problem/C Tags: binary search, math, *1200 Ref: https://codeforces.com/contest/1352/submission/139409987

---
+++ codeforces/1352/C.cpp
@@ -0,0 +1,35 @@
+///*BISMILLAHIR RAHMANIR RAHIM*///
+#include <bits/stdc++.h>
+#define ll long long int
+#define pb push_back
+#define MP make_pair
+#define endl '\n'
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+#define optimize() ios_base :: sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define max_size 1000001
+using namespace std;
+// freopen("input.txt","r",stdin);
+// freopen("output.txt","w",stdout);
+
+/*bool isprime(ll n ){
+       for(ll i=2;i*i<=n;i++){
+        if(n%i==0)return false;
+       }
+       return true;
+}
+*/
+int main()
+{
+  int t;cin>>t;
+  while(t--){
+    ll n,k;cin>>n>>k;
+    ll ans = k+(k/n);
+    ll d = ans/n;ll dd=k/n;
+   while(d-dd!=0){
+     ans+=d-dd;
+    dd = d;
+     d = ans/n;
+   }
+    cout<<ans<<endl;
+  }
+}
