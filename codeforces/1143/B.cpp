e21fabe7157e3357c4bd20573bc70e5cbd50a898
istiak ahmed ovi <istiak6069@gmail.com>
2022-01-07 11:19:00 +0600

    Add solution for problem `B - Nirvana` Link: https://codeforces.com/contest/1143/problem/B Tags: brute force, math, number theory, *1200 Ref: https://codeforces.com/contest/1143/submission/141874831

---
+++ codeforces/1143/B.cpp
@@ -0,0 +1,34 @@
+///*BISMILLAHIR RAHMANIR RAHIM*///
+#include <bits/stdc++.h>
+#define ll long long int
+#define pb push_back
+#define MP make_pair
+#define endl '\n'
+#define F(i, n) for (int i = 0; i < (n); ++i)
+#define optimize() ios_base :: sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define max_size 1000001
+using namespace std;
+typedef vector<int> VI;
+typedef vector<long long int> VLL;
+// freopen("input.txt","r",stdin);
+// freopen("output.txt","w",stdout);
+
+/*bool isprime(ll n ){
+       for(ll i=2;i*i<=n;i++){
+        if(n%i==0)return false;
+       }
+       return true;
+}
+*/
+
+ll solve(ll n){
+  if(n==0)return 1;
+  if(n<10)return n;
+  return max(solve(n/10)*(n%10),solve(n/10-1)*9);
+ }
+
+int main()
+{ //optimize();
+   ll n;cin>>n;
+       cout<< solve(n);
+}
