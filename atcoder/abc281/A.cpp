66fd5d03d3b1028d4895b51a04beb3921f57f1e3
istiak ahmed ovi <istiak6069@gmail.com>
2022-12-10 18:01:00 +0600

    Add solution for problem `A - Count Down` Link: https://atcoder.jp/contests/abc281/tasks/abc281_a Tags: AtCoder, *100 Ref: https://atcoder.jp/contests/abc281/submissions/37138119

---
+++ atcoder/abc281/A.cpp
@@ -0,0 +1,37 @@
+///*BISMILLAHIR RAHMANIR RAHIM*///
+#include <bits/stdc++.h>
+#define ll long long int
+#define pb push_back
+#define MP make_pair
+#define rep(i, n) for (int i = 0; i < n; i++)
+#define rep1(i, n) for (int i = 1; i <= n; i++)
+#define per(i, n) for (int i = n - 1; i >= 0; i--)
+#define per1(i, n) for (int i = n; i >= 1; i--)
+#define clr_1(x) memset(x, -1, sizeof(x));
+#define lb lower_bound
+#define up upper_bound
+#define endl '\n'
+#define optimize()                 \
+    ios_base ::sync_with_stdio(0); \
+    cin.tie(0);                    \
+    cout.tie(0);
+#define max_size 200001
+
+using namespace std;
+typedef vector<int> VI;
+typedef vector<long long int> VLL;
+//// freopen("input.txt","r",stdin);
+// freopen("output.txt","w",stdout);
+
+int drow[] = {-1, 0, 1, 0};
+int dcol[] = {0, 1, 0, -1};
+void solve()
+{
+   int n;cin>>n;
+   for(int i=n;i>=0;i--)cout<<i<<endl;
+}
+int main()
+{
+    optimize();
+    solve();
+}
