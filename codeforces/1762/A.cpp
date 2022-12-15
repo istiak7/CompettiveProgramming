55e4b8b8348b502168cbc1abe474637fda35385a
istiak ahmed ovi <istiak6069@gmail.com>
2022-12-15 21:00:00 +0600

    Add solution for problem `A - Divide and Conquer` Link: https://codeforces.com/contest/1762/problem/A Tags: greedy, math, number theory, *800 Ref: https://codeforces.com/contest/1762/submission/185318847

---
+++ codeforces/1762/A.cpp
@@ -0,0 +1,94 @@
+///*BISMILLAHIR RAHMANIR RAHIM*///
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+#define ll long long int
+#define pb push_back
+#define MP make_pair
+#define rep(i, n) for (int i = 0; i < n; i++)
+#define rep1(i, n) for (int i = 1; i <= n; i++)
+#define per(i, n) for (int i = n - 1; i >= 0; i--)
+#define per1(i, n) for (int i = n; i >= 1; i--)
+#define clr_1(x) memset(x, -1, sizeof(x))
+#define all(v) (v).begin(), (v).end()
+#define rall(v) (v).begin(), (v).end()
+#define lb lower_bound
+#define up upper_bound
+#define endl '\n'
+#define optimize()               \
+  ios_base ::sync_with_stdio(0); \
+  cin.tie(0);                    \
+  cout.tie(0);
+#define max_size 200001
+using namespace std;
+typedef vector<int> VI;
+typedef vector<long long int> VLL;
+
+using namespace __gnu_pbds;
+typedef tree<int, null_type, less<int>, rb_tree_tag,
+             tree_order_statistics_node_update>
+    ordered_set;
+// freopen("input.txt","r",stdin);
+// freopen("output.txt","w",stdout);
+string fact[] = {"", "", "2", "3", "322", "5", "53", "7", "7222", "7332"};
+const int M = 1e5 + 123;
+const ll INF = 2000000000;
+const ll INFLL = 9000000000000000000;
+
+int dx[] = {0, 0, +1, -1, -1, +1, -1, +1};
+int dy[] = {+1, -1, 0, 0, -1, +1, +1, -1};
+const int mx = 2e5 + 123;
+
+void solve()
+{
+  int n;
+  cin >> n;
+  VLL v(n);
+  ll n_odd = 0;
+  rep(i, n)
+  {
+    cin >> v[i];
+    if (v[i] % 2 != 0)
+      n_odd++;
+  }
+
+  if (n_odd % 2 == 0)
+    cout << "0" << endl;
+  else
+  {
+    ll mini = INF;
+    rep(i, n)
+    {
+      if (v[i] % 2 != 0 )
+      {
+        ll cnt = 0;
+        while (v[i] % 2 != 0)
+        {
+          v[i] /= 2;
+          cnt++;
+        }
+        mini = min(cnt, mini);
+      }
+      else{
+        
+        ll cnt = 0;
+        while (v[i] % 2 != 1)
+        {
+          v[i] /= 2;
+          cnt++;
+        }
+        mini = min(cnt, mini);
+      }
+    }
+    cout << mini << endl;
+  }
+}
+
+int main()
+{
+  optimize();
+  int t;
+  cin >> t;
+  while (t--)
+    solve();
+}
