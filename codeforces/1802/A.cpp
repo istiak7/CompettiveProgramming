84dc072e32df9d335fd4a876e58e6c031eb48fc9
istiak ahmed ovi <istiak6069@gmail.com>
2023-03-09 16:01:00 +0600

    Add solution for problem `A - Likes` Link: https://codeforces.com/contest/1802/problem/A Tags: greedy, implementation, *800 Ref: https://codeforces.com/contest/1802/submission/196610259

---
+++ codeforces/1802/A.cpp
@@ -0,0 +1,99 @@
+///*BISMILLAHIR RAHMANIR RAHIM*///
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+#define ll long long int
+#define pb push_back
+#define MP make_pair
+#define rep(i, n) for (int i = 0; i < n; i++)
+#define rep1(i, n) for (int i = 1; i <= n; i++)
+#define per(i, n) for (int i = n - 1; i >= 0; i--)
+#define per1(i, n) for (int i = n; i >= 1; i--)
+#define clr_1(x) memset(x, -1, sizeof(x))
+#define all(v) (v).begin(), (v).end()
+#define rall(v) (v).begin(), (v).end()
+#define SORT(v) sort((v).begin(), (v).end())
+#define lb lower_bound
+#define up upper_bound
+#define endl '\n'
+#define optimize()                 \
+    ios_base ::sync_with_stdio(0); \
+    cin.tie(0);                    \
+    cout.tie(0);
+#define max_size 200001
+#define M 100005
+using namespace std;
+typedef vector<int> VI;
+typedef vector<long long int> VLL;
+
+using namespace __gnu_pbds;
+typedef tree<int, null_type, less<int>, rb_tree_tag,
+             tree_order_statistics_node_update>
+    ordered_set;
+// freopen("input.txt","r",stdin);
+// freopen("output.txt","w",stdout);
+string fact[] = {"", "", "2", "3", "322", "5", "53", "7", "7222", "7332"};
+const ll INF = 2000000000;
+const ll INFLL = 9000000000000000000;
+
+int dx[] = {0, 0, +1, -1, -1, +1, -1, +1};
+int dy[] = {+1, -1, 0, 0, -1, +1, +1, -1};
+const int mx = 2e5 + 123;
+
+void solve()
+{
+    ll n;
+    cin >> n;
+    VLL v(n);
+    rep(i, n)
+    {
+        cin >> v[i];
+    }
+    ll cntp = 0, cntm = 0;
+    rep(i, n)
+    {
+        if (v[i] > 0)
+            cntp++;
+        else
+            cntm++;
+    }
+    //max
+    for (int i = 1; i <= cntp; i++)
+        cout << i << " ";
+    ll m = cntp-1;
+    for (int i = cntm; i >= 1; i--)
+    {
+        if (m > 0)
+            cout << m << " ";
+        else
+            cout << "0 ";
+        m--;
+    }
+    cout << endl;
+
+    //min
+
+    ll mn = min(cntp, cntm);
+    ll mx = max(cntp, cntm);
+    for (int i = 0; i < mn; i++)
+        cout << "1 0 ";
+    if (cntp > cntm)
+    {
+        for (int i = 1; i <= mx - mn; i++)
+            cout << i << " ";
+    }
+    else
+        for (int i = 1; i <= mx - mn; i++)
+            cout << "0 ";
+    cout << endl;
+}
+int main()
+{
+    optimize();
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+}
