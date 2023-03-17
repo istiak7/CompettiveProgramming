98485409fc27ebf8cc352f84063bea888172cdb4
istiak ahmed ovi <istiak6069@gmail.com>
2023-03-17 22:20:00 +0600

    Add solution for problem `A2 - Prefix Flip (Hard Version)` Link: https://codeforces.com/contest/1381/problem/A2 Tags: constructive algorithms, data structures, implementation, strings, two pointers, *1700 Ref: https://codeforces.com/contest/1381/submission/197796507

---
+++ codeforces/1381/A2.cpp
@@ -0,0 +1,121 @@
+///*BISMILLAHIR RAHMANIR RAHIM*///
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+#define ll long long int
+#define pb push_back
+#define MP make_pair
+#define rep(i, n) for (int i = 0; i < n; i++)
+#define rep1(i, n) for (int i = 1; i <= n; i++)
+#define per(i, n) for (int i = n - 1; i >= 0; i--)
+#define per1(i, n) for (int i = n; i >= 1; i--)
+#define clr_1(x) memset(x, -1, sizeof(x))
+#define all(v) (v).begin(), (v).end()
+#define rall(v) (v).begin(), (v).end()
+#define SORT(v) sort((v).begin(), (v).end())
+#define lb lower_bound
+#define up upper_bound
+#define endl '\n'
+#define optimize()               \
+  ios_base ::sync_with_stdio(0); \
+  cin.tie(0);                    \
+  cout.tie(0);
+#define max_size 200001
+#define M 100005
+using namespace std;
+typedef vector<int> VI;
+typedef vector<long long int> VLL;
+
+using namespace __gnu_pbds;
+typedef tree<int, null_type, less<int>, rb_tree_tag,
+             tree_order_statistics_node_update>
+    ordered_set;
+// freopen("input.txt","r",stdin);
+// freopen("output.txt","w",stdout);
+string fact[] = {"", "", "2", "3", "322", "5", "53", "7", "7222", "7332"};
+const ll INF = 2000000000;
+const ll INFLL = 9000000000000000000;
+
+int dx[] = {0, 0, +1, -1, -1, +1, -1, +1};
+int dy[] = {+1, -1, 0, 0, -1, +1, +1, -1};
+const int mx = 2e5 + 123;
+const int N = 10001;
+VLL v;
+void solve()
+{
+  int n;
+  cin >> n;
+  VLL a(n, 0), b(n, 0), v;
+  string s1, s2;
+  cin >> s1;
+  cin >> s2;
+  rep(i, n)
+  {
+    a[i] = s1[i] - '0';
+    b[i] = s2[i] - '0';
+  }
+
+  ll lo = 0, hi = n - 1, bhi = n - 1 , hp = n -1 ;
+  ll op = 0;
+
+  while (lo <= hi)
+  {
+    op++;
+    if (op % 2)
+    {
+      if (b[bhi] != a[lo])
+      {
+        v.pb(hp);
+        hp--;
+        lo++;
+        bhi--;
+      }
+      else
+      {
+        v.pb(0);
+        v.pb(hp);
+        hp--;
+        lo++;
+        bhi--;
+      }
+    }
+    else
+    {
+      if (b[bhi] == a[hi])
+      {
+        v.pb(hp);
+        hp--;
+        hi--;
+        bhi--;
+      }
+      else
+      {
+        v.pb(0);
+        v.pb(hp);
+        hp--;
+        hi--;
+        bhi--;
+      }
+    }
+  }
+
+  cout << v.size() << " ";
+  for (auto u : v)
+      cout << ++u << " ";
+  cout << endl;
+
+  /*rep(i,n)cout << a[i]<<" ";
+  cout << endl ;
+  rep(i,n)cout << b[i]<<" ";
+  cout << endl ;
+  */
+}
+
+int main()
+{
+  optimize();
+  int t;
+  cin >> t;
+  while (t--)
+    solve();
+}
