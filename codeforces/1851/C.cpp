3574fe582cb7540569cab22951e543caf47f0633
istiak ahmed ovi <istiak6069@gmail.com>
2023-07-27 00:59:00 +0600

    Add solution for problem `C - Tiles Comeback` Link: https://codeforces.com/contest/1851/problem/C Tags: greedy, *1000 Ref: https://codeforces.com/contest/1851/submission/215781121

---
+++ codeforces/1851/C.cpp
@@ -0,0 +1,107 @@
+///*BISMILLAHIR RAHMANIR RAHIM*///
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+#define ll long long int
+#define pb push_back
+#define MP make_pair
+#define rep(i, n) for (int i = 0; i < n; i++)
+#define rep1(i, n) for (int i = 1; i <= n; i++)
+#define per(i, n) for (int i = n - 1; i >= 0; i--)
+#define per1(i, n) for (int i = n; i >= 1; i--)
+#define clr_1(x) memset(x, -1, sizeof(x))
+#define all(v) (v).begin(), (v).end()
+#define rall(v) (v).begin(), (v).end()
+#define SORT(v) sort((v).begin(), (v).end())
+#define lb lower_bound
+#define up upper_bound
+#define print(ans)              \
+    for (int i = 0; i < n; i++) \
+        cout << ans[i] << " ";  \
+    cout << endl;
+#define endl '\n'
+#define optimize()                 \
+    ios_base ::sync_with_stdio(0); \
+    cin.tie(0);                    \
+    cout.tie(0);
+#define max_size 200001
+#define M 100005
+#define arr array
+using namespace std;
+typedef vector<int> VI;
+typedef vector<long long int> VLL;
+typedef pair<ll, pair<ll, ll> > pi;
+using namespace __gnu_pbds;
+typedef tree<int, null_type, less<int>, rb_tree_tag,
+             tree_order_statistics_node_update>
+    ordered_set;
+// freopen("input.txt","r",stdin);
+// freopen("output.txt","w",stdout);
+string fact[] = {"", "", "2", "3", "322", "5", "53", "7", "7222", "7332"};
+const ll INF = 2000000000;
+const ll INFLL = 9000000000000000000;
+/*	vector<vector<ll> > a(n,vector<ll>(2));
+        rep(i, n)cin >> a[i][0];
+    pass vec in fun const vector<long long>& a
+*/
+int dx[] = {0, 0, +1, -1, -1, +1, -1, +1};
+int dy[] = {+1, -1, 0, 0, -1, +1, +1, -1};
+int d_row[] = {-1, 0, 1, 0};
+int d_col[] = {0, 1, 0, -1};
+ll const N = 2000000;
+
+void solve()
+{
+    ll n, k;
+    cin >> n >> k;
+    VLL v(n);
+    rep(i, n) cin >> v[i];
+    ll one = 0 ; 
+    if(v[0] == v[n-1]){
+        rep(i,n){
+            if(v[i] == v[0])one++;
+        }
+        if(one >= k){
+            cout << "YES"<<endl;
+        }
+        else cout << "NO" << endl;
+    }
+    else{
+        one = 0 ;
+        ll two = 0 ;
+        ll lo = 0 , hi = n-1;
+        rep(i,n){
+            if(v[0] == v[i]){
+                one++;
+                if(one == k){
+                    lo = i ;
+                    break; 
+                }
+            }
+        }
+        for(int i = n-1 ; i >=0 ; i--){
+            if(v[n-1]==v[i]){
+                two++;
+                if(two == k){
+                    hi = i ;
+                    break;
+                }
+            }
+        }
+        if(lo< hi && one == k && two == k){
+            cout << "YES" << endl;
+        }
+        else cout << "NO" << endl;
+    }
+}
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+}
