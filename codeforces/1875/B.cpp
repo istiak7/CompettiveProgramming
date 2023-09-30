df964eeb50295423b83fb576e85ac721ad7de26c
istiak ahmed ovi <istiak6069@gmail.com>
2023-09-30 22:19:00 +0600

    Add solution for problem `B - Jellyfish and Game` Link: https://codeforces.com/contest/1875/problem/B Tags: brute force, greedy, implementation, *1200 Ref: https://codeforces.com/contest/1875/submission/226021407

--- codeforces/1875/B.cpp
+++ codeforces/1875/B.cpp
@@ -1,162 +1,135 @@
+///*BISMILLAHIR RAHMANIR RAHIM*///
 #include <bits/stdc++.h>
-using namespace std;
-void __print(int x) { cerr << x; }
-void __print(long x) { cerr << x; }
-void __print(long long x) { cerr << x; }
-void __print(unsigned x) { cerr << x; }
-void __print(unsigned long x) { cerr << x; }
-void __print(unsigned long long x) { cerr << x; }
-void __print(float x) { cerr << x; }
-void __print(double x) { cerr << x; }
-void __print(long double x) { cerr << x; }
-void __print(char x) { cerr << '\'' << x << '\''; }
-void __print(const char *x) { cerr << '\"' << x << '\"'; }
-void __print(const string &x) { cerr << '\"' << x << '\"'; }
-void __print(bool x) { cerr << (x ? "true" : "false"); }
- 
-template <typename T, typename V>
-void __print(const pair<T, V> &x)
-{
-    cerr << '{';
-    __print(x.first);
-    cerr << ',';
-    __print(x.second);
-    cerr << '}';
-}
-template <typename T>
-void __print(const T &x)
-{
-    int f = 0;
-    cerr << '{';
-    for (auto &i : x)
-        cerr << (f++ ? "," : ""), __print(i);
-    cerr << "}";
-}
-void _print() { cerr << "]\n"; }
-template <typename T, typename... V>
-void _print(T t, V... v)
-{
-    __print(t);
-    if (sizeof...(v))
-        cerr << ", ";
-    _print(v...);
-}
-#ifndef ONLINE_JUDGE
-#define debug(x...)               \
-    cerr << "[" << #x << "] = ["; \
-    _print(x)
-#else
-#define debug(x...)
-#endif
-#define NEED_FOR_SPEED ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
 #define ll long long int
-#define ull unsigned long long int
-#define vll vector<ll>
-#define vss vector<string>
-#define vpp vector<pair<ll, ll>>
-#define vpc vector<pair<ll, char>>
-#define mll map<ll, ll>
-#define mcc map<char, ll>
-#define sll set<ll>
 #define pb push_back
-#define pob pop_back
-#define gcd(x,y) __gcd(x,y)
-#define all(x) x.begin(), x.end()
-#define sorta(v) sort(all(v))
-#define sortd(v) sort(all(v), greater<ll>())
-#define add(v) accumulate(all(v))
-#define pi M_PI
-#define in insert
-#define rmv erase
-#define ff first
-#define ss second
-#define Yes cout << "YES" << '\n'
-#define No cout << "NO" << '\n'
-#define yes cout << "Yes" << '\n'
-#define no cout << "No" << '\n'
-#define nl cout << '\n'
-#define _TERMINATED_ return 0
+#define MP make_pair
+#define rep(i, n) for (int i = 0; i < n; i++)
+#define rep1(i, n) for (int i = 1; i <= n; i++)
+#define per(i, n) for (int i = n - 1; i >= 0; i--)
+#define per1(i, n) for (int i = n; i >= 1; i--)
+#define clr_1(x) memset(x, -1, sizeof(x))
+#define all(v) (v).begin(), (v).end()
+#define SORT(v) sort((v).begin(), (v).end())
+#define lb lower_bound
+#define up upper_bound
+#define print(ans)              \
+	for (int i = 0; i < n; i++) \
+		cout << ans[i] << " ";  \
+	cout << endl;
+#define endl '\n'
+#define optimize()                 \
+	ios_base ::sync_with_stdio(0); \
+	cin.tie(0);                    \
+	cout.tie(0);
+#define max_size 200001
+#define M 100005
+#define arr array
+using namespace std;
+typedef vector<int> VI;
+typedef vector<long long int> VLL;
+typedef pair<ll, pair<ll, ll>> pi;
+using namespace __gnu_pbds;
+typedef tree<int, null_type, less<int>, rb_tree_tag,
+			 tree_order_statistics_node_update>
+	ordered_set;
+// freopen("input.txt","r",stdin);
+// freopen("output.txt","w",stdout);
+string fact[] = {"", "", "2", "3", "322", "5", "53", "7", "7222", "7332"};
+const ll INF = 2000000000;
+const ll INFLL = 9000000000000000000;
+/*  vector<vector<ll> > a(n,vector<ll>(2));
+		rep(i, n)cin >> a[i][0];
+	pass vec in fun const vector<long long>& a
+*/
+int dx[] = {0, 0, +1, -1, -1, +1, -1, +1};
+int dy[] = {+1, -1, 0, 0, -1, +1, +1, -1};
+int d_row[] = {-1, 0, 1, 0};
+int d_col[] = {0, 1, 0, -1};
+const int N = 200005;
+ll d[N];
 
-void run_case(){
-    ll n, m, k; cin >> n >> m >> k;
-    ll arr[n], brr[m];
-    ll sum1 = 0, sum2 = 0;
-    for(int i = 0; i < n; i++){
-        cin >> arr[i];
-        sum1 += arr[i];
-    }
-    for(int i = 0; i < m; i++){
-        cin >> brr[i];
-        sum2 += brr[i];
-    }
-    sort(arr, arr+n);
-    sort(brr, brr+m);
-
-    int x = 5,cnt = 0;
-    if(k % 2 == 0 ) x = 4 ;
-       while(cnt < x){
-        cnt++;
-        if(cnt % 2 != 0 && arr[0] < brr[m-1]){
-            sum1 -= arr[0];
-            sum1 += brr[m-1];
-            sum2 -= brr[m-1];
-            sum2 += arr[0];
-
-            swap(arr[0], brr[m-1]);
-            sort(arr, arr+n);
-            sort(brr, brr+m);
-        }
-        if(cnt % 2 == 0 && brr[0] < arr[n-1]){
-            sum2 -= brr[0];
-            sum2 += arr[n-1];
-            sum1 -= arr[n-1];
-            sum1 += brr[0];
-
-            swap(brr[0], arr[n-1]);
-            sort(arr, arr+n);
-            sort(brr, brr+m);
-        }
-        if(cnt == x){
-            break;
-        }
-
-
-    //     debug(sum1, sum2, cnt);
-    // }
-    // // debug(p.second);
-    // sorta(v);
-    // ll y = sum1;
-    // // debug(y);
-    // for(int i = 0; i < v.size(); i++){
-    //     if(v[i] == mxsum1){
-    //         if(i - 1 >= 0 && v[i-1] != mxsum1){
-    //             y = v[i-1];
-    //         }
-    //         break;
-    //     }
-    // }
-    // // debug(y);
-    // if((p.second % 2 == 0 && k % 2 == 0) || (p.second % 2 != 0 && k % 2 != 0)){
-    //     cout << mxsum1 << '\n';
-    // }
-    // else{
-    //     cout << y << '\n';
-    // }
-    // return;
-    }
-    cout << sum1 << endl;
-}
- 
-int32_t main()
+void solve()
 {
-    NEED_FOR_SPEED;
-    int t = 1;
-    cin >> t;
-    while(t--){
-        run_case();
-    }
-    _TERMINATED_;
+	ll n, m, k;
+	cin >> n >> m >> k;
+	VLL a(n), b(m);
+	for (int i = 0; i < n; i++)
+	{
+		cin >> a[i];
+	}
+	for (int i = 0; i < m; i++)
+	{
+		cin >> b[i];
+	}
+	SORT(a);
+	SORT(b);
+	ll mx_a = a[n - 1];
+	ll mx_b = b[m - 1];
+	ll sum1 = 0, sum2 = 0, sum3 = 0;
+
+	for (int i = 1; i <= 3; i++)
+	{
+		if (i == 1)
+		{
+			if (a[0] < b[m - 1])
+			{
+				ll temp = a[0];
+				a[0] = b[m - 1];
+				b[m - 1] = temp;
+				SORT(a);
+				SORT(b);
+			}
+			for (int i = 0; i < n; i++)
+			{
+				sum1 += a[i];
+			}
+		}
+		else if (i == 2)
+		{
+			if (b[0] < a[n - 1])
+			{
+				int temp = b[0];
+				b[0] = a[n - 1];
+				a[n - 1] = temp;
+				SORT(a);
+				SORT(b);
+			}
+			for (int i = 0; i < n; i++)
+			{
+				sum2 += a[i];
+			}
+		}
+		else
+		{
+			if (a[0] < b[m - 1])
+			{
+				ll temp = a[0];
+				a[0] = b[m - 1];
+				b[m - 1] = temp;
+				SORT(a);
+				SORT(b);
+			}
+			for (int i = 0; i < n; i++)
+			{
+				sum3 += a[i];
+			}
+		}
+	}
+	if (k == 1)
+		cout << sum1 << endl;
+	else if (k % 2 != 0)
+		cout << sum3 << endl;
+	else
+		cout << sum2 << endl;
 }
-// 3 5 2559324209
-// 815814666 815814666 7188995
-// 192030645 230900474 309067469 7188995 2190953541
+int main()
+{
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	int t;
+	cin >> t;
+	while (t--)
+		solve();
+}
